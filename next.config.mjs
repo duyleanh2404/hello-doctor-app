41c1040bb57285a903522283d61ee8fd3852e689
Le Anh Duy <duyleanh2404@gmail.com>
2024-11-15 19:34:14 +0700

    fix: image, lost key

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
         pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.hellobacsi.com",
+        pathname: "/wp-content/uploads/**"
       }
     ]
   }
--- store/store.ts
+++ store/store.ts
@@ -9,12 +9,12 @@ import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 const encryptTransform = createTransform(
   (inboundState) => {
     const encrypted =
-      CryptoJS.AES.encrypt(JSON.stringify(inboundState), process.env.AES_SECRET_KEY!).toString();
+      CryptoJS.AES.encrypt(JSON.stringify(inboundState), "f4c3d78b5d79b03ac885df66b2b9abcf85f421a123eaf45e1c1e76a7c4f39c51").toString();
     return encrypted;
   },
   (outboundState) => {
     if (!outboundState) return outboundState;
-    const bytes = CryptoJS.AES.decrypt(outboundState, process.env.AES_SECRET_KEY!);
+    const bytes = CryptoJS.AES.decrypt(outboundState, "f4c3d78b5d79b03ac885df66b2b9abcf85f421a123eaf45e1c1e76a7c4f39c51");
     const decrypted = bytes.toString(CryptoJS.enc.Utf8);
     return decrypted ? JSON.parse(decrypted) : null;
   },
